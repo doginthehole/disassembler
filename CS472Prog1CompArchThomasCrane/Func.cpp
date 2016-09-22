4c730898d436c34c1efa274bc6f76c33556dcbdb
BENTLY\Thomas <tcrane12@gmail.com>
2016-09-22 15:53:40 -0400

    Initial commit

---
+++ CS472Prog1CompArchThomasCrane/Func.cpp
@@ -0,0 +1,17 @@
+//if statements for all of the registers with return functions in each
+// or
+//have two different if statements that return an array of the different registers, opcodes, func, or offset
+int bitmask() {
+
+}
+
+//to be called in each of the bitmask for loops
+int bitToHex(int) {
+
+}
+
+//gotta remember to do that thing of making the offset 18 bits
+//you don't need the last two bits as they're always zero, it increments in 4
+int offsetFunc() {
+
+}
